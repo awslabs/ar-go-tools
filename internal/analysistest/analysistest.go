b5b7dbd3c783346985007270bf7c3bad1a76f6a7
Samarth Kishor <samarkis@amazon.com>
2023-06-14 11:56:15 -0400

    Add godoc for analysistest package

--- internal/analysistest/analysistest.go
+++ internal/analysistest/analysistest.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package analysistest contains utility functions for testing the analysis tools.
 package analysistest
 
 import (
